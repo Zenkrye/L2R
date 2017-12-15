2e9b7fe0cc9121935d52fd60e6e8ff725fca1324
Zenkrye <34134715+Zenkrye@users.noreply.github.com>
2017-12-15 11:31:32 -0500

    Add files via upload

---
+++ lib/Images.lua
@@ -0,0 +1,243 @@
+-- Example on cleaning up regions / images
+--PSRL.search.P
+--PSRL.search.S
+--PSRL.search.R
+--PSRL.search.L
+PSR = {
+    QuestsIcon ={
+        P = Pattern("QuestsIcon.png"),
+        S = "QuestsIcon.png",
+        R = Region(870, 35, 100, 100),
+    },
+    Menu ={
+        P = Pattern("Menu.png"),
+        S = "Menu.png",
+        R = Region(839, -44, 150, 150),
+    },
+    WeeklyQuests ={
+        P = Pattern("WeeklyQuests.png"),
+        S = "WeeklyQuests.png",
+        R = Region(-32, 197, 300, 160),
+    },
+    StartQuest ={
+        P = Pattern("StartQuest.png"),
+        S = "StartQuest.png",
+        R = Region(769, 415, 300, 160),
+    },
+    ConnectNow ={
+        P = Pattern("ConnectNow.png"),
+        S = "ConnectNow.png",
+        R = Region(613, 445, 300, 160),
+    },
+    GoNow ={
+        P = Pattern("GoNow.png"),
+        S = "GoNow.png",
+        R = Region(769, 413, 300, 160),
+    },
+    Walk ={
+        P = Pattern("Walk.png"),
+        S = "Walk.png",
+        R = Region(342, 440, 300, 160),
+    },
+    QuestComplete ={
+        P = Pattern("QuestComplete.png"),
+        S = "QuestComplete.png",
+        R = Region(773, 412, 300, 160),
+    },
+    Settings ={
+        P = Pattern("Settings.png"),
+        S = "Settings.png",
+        R = Region(964, -30, 150, 150),
+    },
+    Account ={
+        P = Pattern("Account.png"),
+        S = "Account.png",
+        R = Region(901, 94, 150, 150),
+    },
+    Info ={
+        P = Pattern("Info.png"),
+        S = "Info.png",
+        R = Region(805, 92, 150, 150),
+    },
+    CharacterSelect ={
+        P = Pattern("CharacterSelect.png"),
+        S = "CharacterSelect.png",
+        R = Region(500, 288, 150, 150),
+    },
+    Play ={
+        P = Pattern("Play.png"),
+        S = "Play.png",
+        R = Region(996, 569, 150, 150),
+    },
+    DialogClose ={
+        P = Pattern("DialogClose.png"),
+        S = "DialogClose.png",
+        R = Region(1028, 76, 150, 150),
+    },
+    Done ={
+        P = Pattern("Done.png"),
+        S = "Done.png",
+        R = Region(139, 345, 200, 150),
+    },
+    ToINextFloor ={
+        P = Pattern("ToINextFloor.png"),
+        S = "ToINextFloor.png",
+        R = Region(917, 557, 300, 150),
+    },
+    ToIOK ={
+        P = Pattern("ToIOK.png"),
+        S = "ToIOK.png",
+        R = Region(716, 556, 300, 150),
+    },
+    ToIEnter ={
+        P = Pattern("ToIEnter.png"),
+        S = "ToIEnter.png",
+        R = Region(903, 569, 300, 150),
+    },
+    ToICancel ={
+        P = Pattern("ToICancel.png"),
+        S = "ToICancel.png",
+        R = Region(380, 480, 180, 80),
+    },
+    ToIAutoClear ={
+        P = Pattern("ToIAutoClear.png"),
+        S = "ToIAutoClear.png",
+        R = Region(624, 569, 300, 150),
+    },
+    ToIRecommendedCP ={
+        P = Pattern("ToIRecommendedCP.png"),
+        S = "ToIRecommendedCP.png",
+        R = Region(414, 345, 150, 100),
+    },
+    ToI ={
+        P = Pattern("ToI.png"),
+        S = "ToI.png",
+        R = Region(256, 448, 150, 150),
+    },
+    Use ={
+        P = Pattern("Use.png"),
+        S = "Use.png",
+        R = Region(562, 474, 300, 150),
+    },
+    Dungeon ={
+        P = Pattern("Dungeon.png"),
+        S = "Dungeon.png",
+        R = Region(163, 579, 150, 150),
+    },
+    DailyDungeon ={
+        P = Pattern("DailyDungeon.png"),
+        S = "DailyDungeon.png",
+        R = Region(14, 449, 150, 150),
+    },
+    EliteDungeon ={
+        P = Pattern("EliteDungeon.png"),
+        S = "EliteDungeon.png",
+        R = Region(545, 449, 150, 150),
+    },
+    AntsNest1 ={
+        P = Pattern("AntsNest1.png"),
+        S = "AntsNest1.png",
+        R = Region(16, 370, 150, 150),
+    },
+    Exit ={
+        P = Pattern("Exit.png"),
+        S = "Exit.png",
+        R = Region(1163, -37, 150, 150),
+    },
+    DDEasy ={
+        P = Pattern("DDEasy.png"),
+        S = "DDEasy.png",
+        R = Region(36, 200, 150, 100),
+    },
+    DDNormal ={
+        P = Pattern("DDNormal.png"),
+        S = "DDNormal.png",
+        R = Region(44, 290, 150, 100),
+    },
+    DDHard ={
+        P = Pattern("DDHard.png"),
+        S = "DDHard.png",
+        R = Region(40, 384, 150, 100),
+    },
+    DDVeryHard ={
+        P = Pattern("DDVeryHard.png"),
+        S = "DDVeryHard.png",
+        R = Region(38, 478, 150, 100),
+    },
+    DDEnter ={
+        P = Pattern("DDEnter.png"),
+        S = "DDEnter.png",
+        R = Region(998, 549, 150, 100),
+    },
+    DDOk ={
+        P = Pattern("DDOk.png"),
+        S = "DDOk.png",
+        R = Region(893, 578, 150, 100),
+    },
+    acceptQ ={
+        P = Pattern("acceptQ.png"),
+        S = "acceptQ.png",
+        R = Region(644, 537, 150, 150),
+    },
+    claim ={
+        P = Pattern("claim.png"),
+        S = "claim.png",
+        R = Region(514, 538, 150, 150),
+    },
+    botskip ={
+        P = Pattern("botskip.png"),
+        S = "botskip.png",
+        R = Region(1113, 437, 150, 150),
+    },
+    bubble ={
+        P = Pattern("bubble.png"),
+        S = "bubble.png",
+        R = Region(613, 189, 150, 150),
+    },
+    continueQ ={
+        P = Pattern("continueQ.png"),
+        S = "continueQ.png",
+        R = Region(962, 557, 150, 150),
+    },
+    contok ={
+        P = Pattern("contok.png"),
+        S = "contok.png",
+        R = Region(699, 412, 150, 150),
+    },
+    mqdone ={
+        P = Pattern("mqdone.png"),
+        S = "mqdone.png",
+        R = Region(184, 168, 150, 150),
+    },
+    equip ={
+        P = Pattern("equip.png"),
+        S = "equip.png",
+        R = Region(589, 465, 150, 150),
+    },
+    main ={
+        P = Pattern("main.png"),
+        S = "main.png",
+        R = Region(0, 160, 150, 150),
+    },
+    skiptut ={
+        P = Pattern("skiptut.png"),
+        S = "skiptut.png",
+        R = Region(1116, 30, 150, 75),
+    },
+    ToILeave ={
+        P = Pattern("ToILeave.png"),
+        S = "ToILeave.png",
+        R = Region(1052, 339, 150, 100),
+    },
+    ToIRecharge ={
+        P = Pattern("ToIRecharge.png"),
+        S = "ToIRecharge.png",
+        R = Region(1064, 600, 150, 100),
+    },
+    DDRecharge ={
+        P = Pattern("ToIRecharge.png"),
+        S = "ToIRecharge.png",
+        R = Region(1000, 560, 150, 100),
+    },
+}
+
---
+++ lib/commonLib.lua
@@ -0,0 +1,378 @@
+function immersiveAuto(target)
+    if (getAppUsableScreenSize():getX() == getRealScreenSize():getX()) then
+        setImmersiveMode(true)
+        return true
+    end
+
+    local score = 0
+    setImmersiveMode(false)
+    if (exists(target, 0)) then
+        score = getLastMatch():getScore()
+    else
+        setImmersiveMode(true)
+        if (exists(target, 0)) then return end
+        wait(target, 0)
+    end
+    setImmersiveMode(true)
+    if (exists(target, 0)) then
+        if (getLastMatch():getScore() > score) then return end
+    end
+
+    setImmersiveMode(false)
+end
+
+function regionFinder(target, var)
+    if debugAll == true then toast("Try to find Region") target:highlight(1) end
+    local x = target:getX() - var
+    local y = target:getY() - var
+    local w = target:getW() + (var * 2)
+    local h = target:getH() + (var * 2)
+
+    local orgReg = Region(x,y,w,h)
+
+    if debugAll == true then toast("Found region: "..x..", "..y..", "..w..", "..h) end
+    return orgReg
+end
+
+--[[
+-- Determines the size on which images are matched.
+-- ]]
+function autoResize(target, defaultDimension, immersive, region)
+    local oldROI = Settings:getROI();
+    local max = 0
+    local localX = defaultDimension
+    if (region) then Settings:setROI(region) end
+    if (exists(target, 0)) then
+        resumeROI(oldROI)
+        return defaultDimension
+    end
+
+    usePreviousSnap(true)
+
+    setImmersiveMode(not immersive)
+    if (exists(target, 0)) then
+        resumeROI(oldROI)
+        usePreviousSnap(false)
+        return defaultDimension
+    end
+
+    setImmersiveMode(immersive)
+    target:similar(0.8)
+    local range = defaultDimension * 0.15
+    for x = defaultDimension - range, defaultDimension + range, 10 do
+        Settings:setCompareDimension(true, x)
+        if (exists(target, 0)) then
+            if (getLastMatch():getScore() < max) then
+                localX = x - 10
+                break
+            end
+            max = getLastMatch():getScore()
+        end
+    end
+
+    max = 0
+    for x = (localX - 9), (localX + 9) do
+        Settings:setCompareDimension(true, x)
+        if (exists(target, 0)) then
+            if (getLastMatch():getScore() < max) then
+                Settings:setCompareDimension(true, x - 1)
+                Settings:setScriptDimension(true, x - 1)
+                toast("CompareDimension = "..(x-1))
+                return (x - 1)
+            end
+            max = getLastMatch():getScore()
+        end
+    end
+
+    if (max == 0) then
+        Settings:setCompareDimension(true, defaultDimension)
+        Settings:setScriptDimension(true, defaultDimension)
+        resumeROI(oldROI)
+        usePreviousSnap(false)
+        return -1
+    end
+
+    resumeROI(oldROI)
+    usePreviousSnap(false)
+    return (localX + 9)
+end
+
+-- ============= Image recognition related =============
+function regionWaitMulti(target, seconds, debug, skipLocation)
+    local timer = Timer()
+    local match
+    while (true) do
+        for i, t in ipairs(target) do
+            if (debug) then t.region:highlight(0.5) end
+            if (i == 1) then usePreviousSnap(false) else usePreviousSnap(true) end
+            if ((t.region and (t.region):exists(t.target, 0)) or
+                    (not t.region and exists(t.target, 0))) then -- check once
+                usePreviousSnap(false)
+                if (t.region) then
+                    match = (t.region):getLastMatch()
+                else
+                    match = getLastMatch
+                end
+                if (debug) then match:highlight(0.5) end
+                return i, match, t.id
+            end
+        end
+        if (skipLocation ~= nil) then click(skipLocation) end
+        if (timer:check() > seconds) then
+            usePreviousSnap(false)
+            return -1, "__none__"
+        end
+    end
+end
+
+function waitMulti(target, seconds, skipLocation)
+    local timer = Timer()
+    while (true) do
+        for i, t in ipairs(target) do
+            if (i == 1) then usePreviousSnap(false) else usePreviousSnap(true) end
+            if (exists(t, 0)) then -- check once
+                usePreviousSnap(false)
+                return i, getLastMatch()
+            end
+        end
+        if (skipLocation ~= nil) then click(skipLocation) end
+        if (timer:check() > seconds) then
+            usePreviousSnap(false)
+            return -1
+        end
+    end
+end
+
+function waitMultiReg(target, seconds, skipLocation, reg)
+    local timer = Timer()
+    while (true) do
+        for i, t in ipairs(target) do
+            if (i == 1) then usePreviousSnap(false) else usePreviousSnap(true) end
+            if not reg[i] == nil then
+                if (reg[i]:exists(t, 0)) then
+                    usePreviousSnap(false)
+                    return i, getLastMatch()
+                end
+            else
+                if (exists(t, 0)) then
+                    usePreviousSnap(false)
+                    return i, getLastMatch()
+                end
+            end
+        end
+        if (skipLocation ~= nil) then click(skipLocation) end
+        if (timer:check() > seconds) then
+            usePreviousSnap(false)
+            return -1
+        end
+    end
+end
+
+function waitMultiRegIndex(target, seconds, skipLocation, reg, index, maxIndex)
+    local timer = Timer()
+    while (true) do
+        for i, t in ipairs(target) do
+            if (i == index) or (i == index -1) or (index == maxIndex) then
+                if (i == 1) then usePreviousSnap(false) else usePreviousSnap(true) end
+                if not reg[i] == nil then
+                    if (reg[i]:exists(t, 0)) then
+                        usePreviousSnap(false)
+                        return i, getLastMatch()
+                    end
+                else
+                    if (exists(t, 0)) then
+                        usePreviousSnap(false)
+                        return i, getLastMatch()
+                    end
+                end
+            end
+        end
+        if (skipLocation ~= nil) then click(skipLocation) end
+        if (timer:check() > seconds) then
+            usePreviousSnap(false)
+            return -1
+        end
+    end
+end
+
+function waitMultiClick(target, seconds)
+    local timer = Timer()
+    while (true) do
+        for i, t in ipairs(target) do
+            if (existsClick(t, 0)) then -- check once
+                return i, getLastMatch()
+            end
+        end
+        if (timer:check() > seconds) then return -1 end
+    end
+end
+
+function existsMultiMaxSnap(region, target)
+    local oldROI = Settings:getROI();
+    local maxScore = 0
+    local maxIndex = 0
+    if (region ~= nil) then Settings:setROI(region) end
+    --    region:highlight(2)
+    for i, t in ipairs(target) do
+        if (exists(t, 0)) then -- check once
+            local score = getLastMatch():getScore()
+            if (score > maxScore) then
+                maxScore = score
+                maxIndex = i
+            end
+        end
+    end
+    if (oldROI ~= nil) then
+        Settings:setROI(oldROI)
+    else
+        Settings:setROI()
+    end
+
+    if (maxScore == 0) then return -1 end
+    return maxIndex
+end
+
+function existsMultiMax(target, region)
+    local oldROI = Settings:getROI()
+    local maxScore = 0
+    local maxIndex = 0
+    local match
+    if (region ~= nil) then Settings:setROI(region) end
+    for i, t in ipairs(target) do
+        if (i == 1) then usePreviousSnap(false) else usePreviousSnap(true) end
+        if (exists(t, 0)) then -- check once
+        local score = getLastMatch():getScore()
+        if (score > maxScore) then
+            maxScore = score
+            maxIndex = i
+            match = getLastMatch()
+        end
+        end
+    end
+
+    resumeROI(oldROI)
+    usePreviousSnap(false)
+    if (maxScore == 0) then
+        return -1
+    end
+    return maxIndex, match
+end
+
+function resumeROI(oldROI)
+    if (oldROI) then
+        Settings:setROI(oldROI)
+    else
+        Settings:setROI()
+    end
+end
+
+-- ============= UI related ================
+function simpleDialog(title, message)
+    dialogInit()
+    addTextView(message)
+    dialogShow(title)
+end
+
+function detectLanguage(target, list)
+    local langList = ""
+    for i, l in ipairs(list) do
+        if (exists(target..l..".png", 0)) then return l end
+        langList = langList .. l .."\n"
+    end
+    return (getLanguage())
+end
+
+function offOnScreen(second, pinLock, pin)
+    keyevent(26) -- power
+    wait(second)
+    keyevent(82) --unlock
+    if (pinLock) then
+        type(pin) --passcode
+        keyevent(66) -- enter
+    end
+end
+
+-- ============= strings related ================
+function fileExists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
+function loadStrings(path)
+    local language = getLanguage()
+    local file = path.."strings."..getLanguage()..".lua";
+    if (fileExists(file)) then
+        dofile(file)
+    else
+        if (fileExists(path.."strings.lua")) then
+            dofile(path.."strings.lua")
+        end
+    end
+end
+
+-- ============= Lua language related =============
+function tableLookup(table, item)
+    for i, t in ipairs(table) do
+        if (t == item) then return i end
+    end
+    return -1
+end
+
+function tableLength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+
+-- ========== SWAR-X specific Functions ==========
+function isPro()
+    if string.find(getVersion(), "pro" ) then
+        return true
+    else
+        return false
+    end
+end
+function isLatestVersion()
+    if string.find(getVersion(), "6.8.0" ) then
+        return true
+    else
+        return false
+    end
+end
+-- Allow new functionality to partially be supported in the script
+function isSupportedDimension()
+    -- ## CHANGING THIS MAY BREAK THE SCRIPT ##
+    if resChoice == "2560x1600" or resChoice == "1920x1200" or resChoice == "1280x800"  then
+        return true
+    else
+        return false
+    end
+end
+function showStatsSection(shouldShow)
+    ---Screen Stats
+    statsSection:highlightOff()
+    wait(.1)
+
+    if shouldShow then
+        statsSection:highlight(
+            "Runs: \n"..
+            "T: "..tostring(runsCount).." | V: "..tostring(victoryCount).." | D: "..tostring(deathCount).."\n"..
+            "\n"..
+            "Runes: \n"..
+            "B: "..tostring(runesKeptCount).." | S: "..tostring(runesSoldCount)
+        )
+    end
+
+end
+function stageDetect()
+
+
+end
+function stageSelect()
+
+
+end
+function levelSelect()
+
+
+end
